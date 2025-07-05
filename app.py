6c7648272fe301332feed949add9240929a24835
pakapongh <pakapongh@gmail.com>
2025-07-05 16:18:30 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+from flask import Flask, render_template, request
+import os
+import uuid
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    uploaded_image_url = None
+
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            result = "ไม่พบไฟล์ภาพที่แนบมา"
+            return render_template('index.html', result=result)
+
+        file = request.files['image']
+        if file.filename == '':
+            result = "คุณยังไม่ได้เลือกไฟล์"
+            return render_template('index.html', result=result)
+
+        if file:
+            filename = f"{uuid.uuid4().hex}.jpg"
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            uploaded_image_url = path
+
+            image = cv2.imread(path)
+            if image is not None:
+                bgr_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+                h_half, w_half = 300, 700
+                x_center, y_center = bgr_image.shape[0] // 2, bgr_image.shape[1] // 2
+
+                x1 = max(x_center - h_half, 0)
+                x2 = min(x_center + h_half, bgr_image.shape[0])
+                y1 = max(y_center - w_half, 0)
+                y2 = min(y_center + w_half, bgr_image.shape[1])
+
+                roi = bgr_image[x1:x2, y1:y2]
+
+                if roi.size == 0:
+                    result = "ไม่สามารถอ่านค่าจากภาพได้ (ROI ว่างหรืออยู่นอกขอบภาพ)"
+                else:
+                    blue_channel = roi[:, :, 2]
+                    blue_intensity = np.mean(blue_channel)
+
+                    if blue_intensity <= 166.91:
+                        intensity_info = "น้อยมาก (0 - 0.039)"
+                    elif 166.92 <= blue_intensity <= 183.25:
+                        intensity_info = "น้อย (0.78 - 1.56)"
+                    elif 183.26 <= blue_intensity <= 187.58:
+                        intensity_info = "ปานกลาง (1.57 - 3.125)"
+                    elif 187.59 <= blue_intensity <= 194.08:
+                        intensity_info = "สูง (3.126 - 6.25)"
+                    else:
+                        intensity_info = "สูงมาก (6.26 - 50)"
+
+                    result = f"ความเข้มข้นของวิตามินซี: {intensity_info} มิลลิกรัมต่อมิลลิลิตร"
+            else:
+                result = "ไม่สามารถโหลดไฟล์ภาพได้"
+
+    return render_template('index.html', result=result, image_path=uploaded_image_url)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
